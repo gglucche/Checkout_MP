c7cec4e16461d718068869a7c3b36fa0314f4690
gglucche <gglucche@gmail.com>
2017-10-27 07:48:29 -0200

    Add files via upload

--- notification.php
+++ notification.php
@@ -2,9 +2,10 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
+require_once "../conexao9.php";
 require_once "mercadopago.php";
 
-$mp = new MP("DADO__PESSOAL");
+$mp = new MP("SECRETO");
 
 $json_event = file_get_contents('php://input', true);
 $event = json_decode($json_event);
@@ -19,12 +20,13 @@ if ($event->type == 'payment'){
 
     if ($payment_info["status"] == 200) {
         print_r($payment_info["response"]);
-        $myfile = fopen("MP/notifications.txt", "w") or die("falha ao gerar arquivo!");
+        $myfile = fopen("notifications.txt", "w") or die("falha ao gerar arquivo!");
 		$txt = "payment_info:\n";
 		fwrite($myfile, $txt);
 		$txt = serialize($payment_info);
 		fwrite($myfile, $txt);
 		fclose($myfile);
+		$updt = mysql_query("UPDATE `starkclub_pf` SET `payment_info` = '".$payment_info["response"]."' WHERE `starkclub_pf`.`id_trans` = '".$_GET['id']."'") or die(mysql_error());
     }
 }
 	
